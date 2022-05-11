21b7362fd197ca27e3cb5e3c77a7a5c5e15fb0d7
noverd <dasfasru@gmail.com>
2022-05-11 07:53:23 +0300

    SETUp update

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup
-
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 setup(name='scratchplus',
-      version='0.9',
+      version='0.911',
       description='API wrapper for scratch',
       packages=['scratchplus'],
       author_email='thefinalspacestudio@gmail.com',
@@ -9,4 +11,7 @@ setup(name='scratchplus',
       zip_safe=False,
       url='https://github.com/noverd/ScratchPlus',
       install_requires=["requests",
-                        "websoket", "pymitter"])
+                        "websoket", "pymitter"],
+      long_description=long_description,
+      long_description_content_type='text/markdown'
+      )
